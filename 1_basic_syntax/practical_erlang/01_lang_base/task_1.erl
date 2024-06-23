736b6e9f935f0685cfc40c8b19498c9c952ac43f
Oleg Shipulin <olegshipu95@gmail.com>
2024-06-23 15:46:19 +0300

    update

---
+++ 1_basic_syntax/practical_erlang/01_lang_base/task_1.erl
@@ -0,0 +1,30 @@
+%%%-------------------------------------------------------------------
+%%% @author olegshipu
+%%% @copyright (C) 2024, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 16. Jun 2024 19:41
+%%%-------------------------------------------------------------------
+-module(task_1).
+-author("olegshipu").
+
+-export([is_in_range/3]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+
+%% Check is Val in range [From, To], both edges included
+is_in_range(Val, Left, Right) ->
+  false.
+
+is_in_range_test() ->
+  ?assertEqual(true, is_in_range(5, 2, 10)),
+  ?assertEqual(true, is_in_range(5, 5, 10)),
+  ?assertEqual(true, is_in_range(5, 1, 5)),
+  ?assertEqual(false, is_in_range(50, 1, 5)),
+  ?assertEqual(false, is_in_range(-5, 1, 5)),
+  ?assertEqual(true, is_in_range(1, 1, 1)),
+  ?assertEqual(false, is_in_range(1, 5, 1)),
+  ok.
---
+++ 1_basic_syntax/practical_erlang/01_lang_base/test.erl
@@ -0,0 +1,29 @@
+%%%-------------------------------------------------------------------
+%%% @author olegshipu
+%%% @copyright (C) 2024, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 16. Jun 2024 19:44
+%%%-------------------------------------------------------------------
+-module(test).
+
+-export([run/0, run/1]).
+
+run() ->
+  Modules = [task_1, task_2, task_3, task_4, task_5],
+  Res = lists:map(fun(M) -> eunit:test(M) end, Modules),
+  case Res of
+    [ok,ok,ok,ok,ok] -> init:stop(0);
+    _ -> init:stop(1)
+  end.
+
+
+run([M, F]) ->
+  io:format("run test ~p:~p~n", [M, F]),
+  Res = erlang:apply(M, F, []),
+  io:format("result: ~p~n~n", [Res]),
+  case Res of
+    ok -> init:stop(0);
+    error -> init:stop(1)
+  end.
