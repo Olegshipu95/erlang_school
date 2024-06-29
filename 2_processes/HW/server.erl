58e0b52c654e6924c456b61a3d98d20c0e70e295
Oleg Shipulin <olegshipu95@gmail.com>
2024-06-30 00:48:34 +0300

    rewrite hw without testing

--- 2_processes/HW/server.erl
+++ 2_processes/HW/server.erl
@@ -34,7 +34,7 @@ handle_connection() ->
   receive
     {calculate, From, Data} when is_pid(From) andalso is_tuple(Data) ->
       io:format("Take information from ~p value ~p~n", [From, Data]),
-      Result = operation_validation(Data),
+      Result = success_operation(Data),
       From ! {self(), Result},
       handle_connection();
     stop ->
@@ -46,31 +46,40 @@ handle_connection() ->
       handle_connection()
   end.
 
--spec operation_validation({atom(), [number()]}) -> {ok, number()} | {error, iolist()}.
-operation_validation({RequestOperation, Args})  when is_atom(RequestOperation) andalso is_list(Args)->
-  {Code, MyOperation} = operation(RequestOperation),
-  case Code of
-    ok ->
-      io:format("My operation is - ~p, my args - ~p~n", [MyOperation, Args]),
-      {ok, divider_args(MyOperation, Args, 0)};
-    _  -> {Code, MyOperation}
-  end;
-operation_validation(Data) -> {error, io_lib:format("unknown data ~p~n", [Data])}.
+-spec success_operation({atom(), [number()]}) -> {ok, number()} | {error, iolist() | {error, atom()}}.
+success_operation(Data = {RequestOperation, Args}) when is_atom(RequestOperation) andalso is_list(Args) ->
+  ReturnResult = try operation_validation(Data) of
+                   ResultOfOperation -> {ok, ResultOfOperation}
+                 catch
+                   Throw -> {error, Throw}
+                 end,
+  ReturnResult;
+success_operation(Data) -> {error, io_lib:format("unknown data ~p~n", [Data])}.
 
--spec divider_args(atom(), [number()], number()) -> number().
-divider_args(_, [], Acc) -> Acc;
-divider_args(Operation, [A|Tail], Acc) ->
-  io:format("Step_divider: Acc is ~p, A is ~p, Tail is ~p~n", [Acc, A, Tail]),
-  divider_args(Operation, Tail, erlang:apply(erlang, Operation, [A,Acc])).
 
--spec operation(atom()) -> {ok, atom()} | {error, iolist()}.
-operation(add) -> {ok, '+'};
-operation(sub) -> {ok, '-'};
-operation(mul) -> {ok, '*'};
-operation(division) -> {ok, '/'};
-operation(remainder) -> {ok, 'rem'};
-operation(AnotherAtom) ->
-  {error, io_lib:format("Invalid operation: ~p~n", [AnotherAtom])}.
+-spec operation_validation({atom(), [number()]}) -> {ok, number()}.
+operation_validation({RequestOperation, Args}) when is_atom(RequestOperation) andalso is_list(Args) ->
+  {Code, Fun} = operation(RequestOperation),
+  Result = case Code of
+             ok ->
+               io:format("My operation is - ~p, my args - ~p~n", [Fun, Args]),
+               Fun(Args);
+             _ -> throw(incorrect_operation)
+           end,
+  Result;
+operation_validation(X) when is_integer(X) -> X;
+operation_validation(_Data) -> throw(incorrect_data).
+
+
+-spec operation(atom()) -> {ok, Fun} | {error, iolist()}.
+operation(add) -> {ok, fun(Args) -> lists:foldl(fun(Arg, Acc) -> operation_validation(Arg) + Acc end, 0, Args) end};
+operation(sub) ->
+  {ok, fun(Args) -> lists:foldl(fun(Arg, Acc) -> Acc - operation_validation(Arg) end, hd(Args), tl(Args)) end};
+operation(mul) -> {ok, fun(Args) -> lists:foldl(fun(Arg, Acc) -> operation_validation(Arg) * Acc end, 1, Args) end};
+operation(division) ->
+  {ok, fun(Args) -> lists:foldl(fun(Arg, Acc) -> Acc / operation_validation(Arg) end, hd(Args), tl(Args)) end};
+operation(Data) ->
+  {error, io_lib:format("Invalid operation: ~p~n", [Data])}.
 
 -spec get_all_pids() -> [pid()].
 get_all_pids() ->
